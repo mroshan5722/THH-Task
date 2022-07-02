7eb1fda92036e58a121712c95e2b2976b0cba81d
mroshan5722 <mroshan5722@gmail.com>
2022-07-02 23:58:28 +0400

    php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$host = "localhost"; 
+$user = "root"; 
+$pass = ""; 
+$db = "puzzledb";
+
+$conn = mysqli_connect("localhost", "root", "", "puzzledb");
+
+// Check connection
+if($conn === false){
+    die("ERROR: Could not connect. "
+        . mysqli_connect_error());
+}
+
+// Taking all 5 values from the form data(input)
+$Name = $_POST['name'];
+$Email = $_POST['email'];
+$Mobile = $_POST['mobile'];
+
+// Performing insert query execution
+// here our table name is college
+$sql = "INSERT INTO users VALUES ('id','$Name',
+    '$Email','$Mobile')";
+
+if(mysqli_query($conn, $sql)){
+    echo "Contact Records Inserted";
+}
+
+// Close connection
+mysqli_close($conn);
+?>
